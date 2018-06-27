4800a032350d3fc4dfe692d710e69a6789386ee1
Yaric Pisarev <yaricp@gmail.com>
2018-06-27 20:44:47 +0700

    setup with version of system. added readme.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # eeg-filters
+
+this program allow to analyze curves of EEG on different frequencies.
+
+Installation was tested on ubuntu 18.04
+
+For Installation run ./setup.sh with root permissions.
+
+For start programm run ./start.sh
--- setup.sh
+++ setup.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
-echo 'install virtualenv'
-sudo apt install -y virtualenv
+echo 'check linux distro'
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+distro="$(lsb_release -r)"
+if [[ ${machine} = 'Linux' ]]
+then
+  echo 'install virtualenv'
+  if echo ${distro} | grep -q "18.04";
+  then
+    echo ${distro}
+    sudo apt install -y virtualenv
+  eslif echo ${distro} | grep -q "16.04";
+    sudo apt-get install python-pip
+    sudo pip install virtualenv
+  fi
+fi
+
+
 virtualenv venv
 echo 'install python requirements'
 venv/bin/pip install -r requirements.txt
